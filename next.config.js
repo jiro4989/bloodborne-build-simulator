f21aa4ac5046b966a405a7f7d335ced296ca9ab6
jiro4989 <jiroron666@gmail.com>
2023-12-03 18:50:59 +0900

    fix: assetPrefixを追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: process.env.ASSET_PREFIX,
   output: 'export', // 静的ファイル出力を有効化
   reactStrictMode: true,
   swcMinify: true,
