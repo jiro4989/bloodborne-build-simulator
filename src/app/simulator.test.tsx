95fffd151f6126c372da9a8446710ceb3fc8fea6
jiro4989 <jiroron666@gmail.com>
2024-12-30 18:40:09 +0900

    fix: とりあえずテストが通るようにした

--- src/app/simulator.test.tsx
+++ src/app/simulator.test.tsx
@@ -1,9 +1,16 @@
-import { expect, test } from 'vitest'
+import { expect, test, vi, vitest } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Simulator from './simulator'
 import { describe } from 'node:test'
 
 describe('Simulator', () => {
+    vi.mock('next/navigation', () => {
+        return {
+            useSearchParams: vitest.fn().mockReturnValue(new URLSearchParams({
+            }))
+        }
+    })
+
     render(<Simulator />)
 
     test('Title', () => {
