ed8e08047a5bb534d2376091a70f59c1f10c9e0e
jiro4989 <jiroron666@gmail.com>
2024-12-30 18:42:12 +0900

    fix: test に mock と render を移動

--- src/app/simulator.test.tsx
+++ src/app/simulator.test.tsx
@@ -4,16 +4,15 @@ import Simulator from './simulator'
 import { describe } from 'node:test'
 
 describe('Simulator', () => {
-    vi.mock('next/navigation', () => {
-        return {
-            useSearchParams: vitest.fn().mockReturnValue(new URLSearchParams({
-            }))
-        }
-    })
-
-    render(<Simulator />)
-
     test('Title', () => {
+        vi.mock('next/navigation', () => {
+            return {
+                useSearchParams: vitest.fn().mockReturnValue(new URLSearchParams({
+                }))
+            }
+        })
+
+        render(<Simulator />)
         expect(screen.getByRole('heading', { level: 1, name: 'Bloodborne ビルドシミュレータ' })).toBeDefined()
     })
 })
